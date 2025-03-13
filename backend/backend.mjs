a14936e7c3664577ecc24cb44928c4da290e4068
Lacroix-Eva <eva.lacroix@edu.univ-fcomte.fr>
2025-03-13 01:11:47 +0100

    Gros avancement

---
+++ backend/backend.mjs
@@ -0,0 +1,144 @@
+import PocketBase from 'pocketbase' ;
+const pb = new PocketBase('http://127.0.0.1:8090') ;
+
+// 1) Retourne la liste de tous les films triés par date de projection
+
+export async function getAllMoviesSortedByDate() {
+    let records = await pb.collection('Film').getFullList({sort: 'date' });
+    records = records.map((film) => {
+            film.img = pb.files.getURL(film, film.affiche);
+            return film;
+        });
+    return records;
+}
+
+
+// 2) Retourne la liste des activités (Ateliers, expositions, animations…) triés par date de projection
+
+export async function getAllActivitesSortedByDate() {
+    const records = await pb.collection('Activite').getFullList({
+        sort: 'date',
+    });
+    return records;
+}
+
+
+// 3) Retourne la liste de tous les acteurs / réalisateurs participant au festival triés par ordre alphabétique
+
+export async function getAllActeursAndRealisateursSortedByNom() {
+    const records = await pb.collection('Invite').getFullList({sort: 'nom'}, 
+        {filter: "role = 'Réalisateur'||role = 'Acteur'"});
+    return records;
+}
+
+// En Plus) Retourne la liste de tous les invités (animateurs, acteurs, réalisateurs) triés par ordre alphabétique
+
+export async function getAllInvitesSortedByNom() {
+    const records = await pb.collection('Invite').getFullList({sort: 'nom'});
+    return records;
+}
+
+// 4) Retourne les infos d'un film en donnant son id en paramètre
+
+export async function getFilmById(id) {
+    const records = await pb.collection('Film').getOne(id);
+    return records;
+}
+
+
+// 5) Retourne les infos d'une activité en donnant son id en paramètre
+
+export async function getActivitesById(id) {
+    const records = await pb.collection('Activite').getOne(id);
+    return records;
+}
+
+
+// 6) Retourne les infos d'un acteur / réalisateur en donnant son id en paramètre
+
+export async function getInviteById(id) {
+    const records = await pb.collection('Invite').getOne(id);
+    if (records.role === 'Animateur') {
+        return "Cet invité est un animateur";
+    }
+    return records;
+}
+
+// En Plus) Retourne les infos d'un invité en donnant sont id en paramètre
+
+export async function getInviteBisById(id) {
+    const records = await pb.collection('Invite').getOne(id);
+    return records;
+}
+
+
+// 7) Retourne toutes les activités d’un animateur donné par son id
+
+export async function getActiviteByAnimateurId(id) {
+    const records = await pb.collection('Activite').getFullList(
+        {filter: `invite.id ='${id}'`, expand: 'Invite'});
+    return records;
+}
+
+// En Plus) Retourne tous les films d’un animateur donné par son id
+
+export async function getFilmByAnimateurId(id) {
+    const records = await pb.collection('Film').getFullList(
+        {filter: `invite.id ='${id}'`, expand: 'Invite'});
+    return records;
+}
+
+
+// 8) Retourne toutes les activités d’un animateur donné par son nom
+export async function getActiviteByAnimateurName(nom) {
+    const records = await pb.collection('Activite').getFullList({
+        filter: `invite.nom ='${nom}'`, expand: 'Invite'
+    });
+    return records;
+}
+
+
+// 9) Permet d’ajouter ou modifier les informations d’un film, activité ou invité
+
+export async function addNewFilm(newFilm) {
+    await pb.collection('Film').create(newFilm);
+}
+
+export async function addNewActivite(newActivite) {
+    await pb.collection('Activite').create(newActivite);
+}
+
+export async function addNewInvite(newInvite) {
+    await pb.collection('Invite').create(newInvite);
+}
+
+export async function updateFilmById(id, newFilm) {
+    const records = await pb.collection("Film").update(id, newFilm) ;
+    return records ;
+}
+
+export async function updateActiviteById(id, newActivite) {
+    const records = await pb.collection("Activite").update(id, newActivite) ;
+    return records ;
+}
+
+export async function updateInviteById(id, newInvite) {
+    const records = await pb.collection("Invite").update(id, newInvite) ;
+    return records ;
+}
+
+// Exportation de l'instance PocketBase pour l'utiliser dans d'autres fichiers
+export { pb };
+
+// En Plus) Filtre films par genre
+export async function getOffresByGenre(g) {
+    const FilmGenre = await pb.collection('Film').getFullList({
+        filter: `genre = "${g}"`, // Ajout des guillemets pour éviter une erreur
+    });
+
+    FilmGenre.forEach((film) => {
+        film.img = pb.files.getURL(film, film.images);
+    });
+
+    return FilmGenre;
+}
---
+++ backend/testback.js
@@ -0,0 +1,149 @@
+// import { getAllMoviesSortedByDate } from './backend.mjs';
+// import { getAllActivitesSortedByDate } from './backend.mjs';
+// import { getAllActeursAndRealisateursSortedByNom } from './backend.mjs';
+// import { getFilmById } from './backend.mjs';
+// import { getActivitesById } from './backend.mjs';
+// import { getInviteById } from './backend.mjs';
+// import { getActiviteByAnimateurId } from './backend.mjs';
+// import { getActiviteByAnimateurName } from './backend.mjs';
+// import { addNewFilm } from './backend.mjs';
+// import { addNewActivite } from './backend.mjs';
+// import { addNewInvite } from './backend.mjs';
+// import { updateFilmById } from './backend.mjs';
+// import { updateActiviteById } from './backend.mjs';
+// import { updateInviteById } from './backend.mjs';
+// import { getInviteBisById } from './backend.mjs';
+import { getFilmByAnimateurId } from './backend.mjs';
+
+// try {
+//     const records = await getAllMoviesSortedByDate() ;
+//     console.log(records) ;
+// } catch (e) {
+//     console.error(e) ;
+// }
+
+// try {
+//     const records = await getAllActivitesSortedByDate() ;
+//     console.log(records) ;
+// } catch (e) {
+//     console.error(e) ;
+// }
+
+// try {
+//     const records = await getAllActeursAndRealisateursSortedByNom() ;
+//     console.log(records) ;
+// }
+// catch (e) {
+//     console.error(e) ;
+// }
+
+// try {
+//     const records = await getFilmById('jo4re7zg4379x81') ;
+//     console.log(records) ;
+// }
+// catch (e) {
+//     console.error(e) ;
+// }
+
+// try {
+//     const records = await getActivitesById('h85ej2j54weea15') ;
+//     console.log(records) ;
+// }
+// catch (e) {
+//     console.error(e) ;
+// }
+
+// try {
+//     const records = await getInviteById('z3gi44g72yqpnja') ;
+//     console.log(records) ;
+// }
+// catch (e) {
+//     console.error(e) ;
+// }
+
+// try {
+//     const records = await getActiviteByAnimateurId('z3gi44g72yqpnja') ;
+//     console.log(records) ;
+// } catch (e) {
+//     console.error(e) ;
+// }
+
+// try {
+//     const records = await getActiviteByAnimateurName('Bochard') ;
+//     console.log(records) ;
+// }
+// catch (e) {
+//     console.error(e) ;
+// }
+
+// try {
+//     const newFilm = {
+//     "titre": "test ajout"
+// };
+
+//     await addNewFilm(newFilm);
+// }catch (e) {
+//     console.error(e);
+// }
+
+// try {
+//     const newActivite = {
+//     "titre": "test ajout"
+// };
+
+//     await addNewActivite(newActivite);
+// }catch (e) {
+//     console.error(e);
+// }
+
+// try {
+//     const newInvite = {
+//     "nom": "test ajout"
+// };
+
+//     await addNewInvite(newInvite);
+// }catch (e) {
+//     console.error(e);
+// }
+
+// try {
+//     const newFilm = {
+//         "titre": "The Mist test update"
+//     }
+//     await updateFilmById("3k5fa4asd0j3u3v", newFilm) ;
+// } catch (e) {
+//     console.error(e) ;
+// }
+
+// try {
+//     const newActivite = {
+//         "titre": "Découverte du cinéma Super 8 test update"
+//     }
+//     await updateActiviteById("72ia8svbr317qd4", newActivite) ;
+// } catch (e) {
+//     console.error(e) ;
+// }
+
+// try {
+//     const newInvite = {
+//         "nom": "Hanks test update"
+//     }
+//     await updateInviteById("87t07x7o7a5a2a6", newInvite) ;
+// } catch (e) {
+//     console.error(e) ;
+// }
+
+// try {
+//     const records = await getInviteBisById('ugnwds90vhj6329') ;
+//     console.log(records) ;
+// }
+// catch (e) {
+//     console.error(e) ;
+// }
+
+// try {
+//     const records = await getFilmByAnimateurId('hrsd12o6ddphp86') ;
+//     console.log(records) ;
+// } catch (e) {
+//     console.error(e) ;
+// }
